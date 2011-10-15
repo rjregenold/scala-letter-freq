662bb37c473c5df84ed56ff5facd28be3fe799c5
RJ Regenold <rjregenold@gmail.com>
2011-10-15 17:03:32 -0500

    Got rid of mutable map. Woot.

--- src/main/scala/com/binarylion/letters/Letters.scala
+++ src/main/scala/com/binarylion/letters/Letters.scala
@@ -3,47 +3,29 @@ package letters
 
 import scala.actors.Future
 import scala.actors.Futures._
-import scala.collection.mutable.Map
 import scala.io.Source
 
 object Letters {
   def emptyList = Map[Char, Int]().withDefaultValue(0)
 
-  /**
-   * Converts a word into a map with characters for keys
-   * and the number of times that character appears in the
-   * word as values. For example, 'zoo' would produce:
-   *
-   * Map('z' -> 1, 'o' -> 2)
-   *
-   * @param word the word to convert
-   * @return a map containing the count of each letter in the word
-   */
-  def convertWord(word:String) = {
-    (emptyList /: word.toLowerCase) {
-      case (a, c) => a ++ Map(c -> (a(c) + 1))
-    }
+  def makeLetterMap(word:String) = (emptyList /: word.toLowerCase) {
+    case (acc, letter) => acc + (letter -> (acc(letter) + 1))
   }
 
-  /**
-   * Takes a List of Map[Char, Int] and folds them into a single
-   * Map[Char, Int].
-   */
-  def foldMaps(maps:TraversableOnce[Map[Char, Int]]) = (emptyList /: maps) {
-    case (acc, map) =>
-      for((key, value) <- map) acc(key) += value
-      acc
+  def foldLetterMaps(letterMaps:TraversableOnce[Map[Char, Int]]) = (emptyList /: letterMaps) {
+    case (acc, letterMap) =>
+      acc ++ letterMap.map{case (letter, count) => (letter -> (acc(letter) + count))}
   }
 
   def main(args:Array[String]):Unit = {
     val fs = (0 to 3) map { (idx) =>
       future {
         val h = Source.fromFile("src/main/resources/english." + idx)
-        foldMaps(h.getLines.filterNot(_ contains '\'').map(convertWord(_)))
+        foldLetterMaps(h.getLines.filterNot(_ contains '\'').map(makeLetterMap(_)))
       }
     }
 
-    val result = foldMaps(fs.map(_()))
+    val result = foldLetterMaps(fs.map(_()))
     val totalChars = result.values.sum
     println("Total letters: " + totalChars)
     result.keys.toList.sorted foreach { (letter) => 
