00779741ba279e040bc7c2c17186f3d0559008bc
RJ Regenold <rjregenold@gmail.com>
2011-10-17 21:35:48 -0500

    Added comments.

--- src/main/scala/com/binarylion/letters/Letters.scala
+++ src/main/scala/com/binarylion/letters/Letters.scala
@@ -5,13 +5,37 @@ import scala.actors.Futures.future
 import scala.io.Source
 
 object Letters {
-  def emptyMap = Map[Char, Int]().withDefaultValue(0)
+  type LetterMap = Map[Char, Int]
 
+  def emptyMap:LetterMap = Map().withDefaultValue(0)
+
+  /**
+   * Takes a word and converts it into a Map with its letters as keys
+   * and the number of times each letter appears in the word as values.
+   * For example, the word 'zoo' would produce the following LetterMap:
+   *
+   *  Map('z' -> 1, 'o' -> 2)
+   *
+   * @param word the word to convert into a LetterMap
+   * @return a populated LetterMap for the word
+   */
   def makeLetterMap(word:String) = (emptyMap /: word.toLowerCase) {
     case (acc, letter) => acc + (letter -> (acc(letter) + 1))
   }
 
-  def foldLetterMaps(letterMaps:TraversableOnce[Map[Char, Int]]) = (emptyMap /: letterMaps) {
+  /**
+   * Takes a list of LetterMaps and folds them into a single LetterMap.
+   *
+   *  List(Map('z' -> 1, 'o' -> 2), Map('j' -> 1, 'o' -> 1, 'y' -> 1))
+   * 
+   * would produce:
+   *
+   *  Map('z' -> 1, 'o' -> 3, 'j' -> 1, 'y' -> 1)
+   *
+   * @param letterMaps the list of LetterMaps
+   * @return a single LetterMap with keys/values combined from the LetterMaps
+   */
+  def foldLetterMaps(letterMaps:TraversableOnce[LetterMap]) = (emptyMap /: letterMaps) {
     case (acc, letterMap) =>
       acc ++ letterMap.map{case (letter, count) => (letter -> (acc(letter) + count))}
   }
@@ -26,7 +50,7 @@ object Letters {
 
     val result = foldLetterMaps(fs.map(_()))
     val totalLetters = result.values.sum
-    println("Total letters: " + totalLetters)
+    println("Total letters: %d" format totalLetters)
     result.keys.toList.sorted foreach { (letter) => 
       val freq = (result(letter).toDouble / totalLetters.toDouble) * 100
       println("%s:\t%d\t\t%.2f%%" format (letter, result(letter), freq))
